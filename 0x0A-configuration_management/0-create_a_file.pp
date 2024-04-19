b6d2151a5f51a8332ce2e976829562888cb8519a
mooreArrqs <amandanxumalo921@gmail.com>
2024-04-19 04:10:22 -0700

    Creating a file using Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creating a file named school inside the /tmp directory
+
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode	  => '0744',
+  owner	  => 'www-data',
+  group   => 'www-data'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,3 @@
+0x0A. Configuration management
+
+This project introduces a topic on configuration management, with a focus on the tool Puppet, which is an automation that fixes/restore cloud infastructure.
