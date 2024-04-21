6a29f30670f1c5f37d09cfb407b3c80cd238f302
mooreArrqs <amandanxumalo921@gmail.com>
2024-04-20 17:41:12 -0700

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 # creating a manifest that kills a process named killmenow
 exec { 'killmenow':
-  command  => 'pkill killmenow',
+  command => 'pkill killmenow',
   path    => '/bin:/usr/bin',
   onlyif  => 'pgrep killmenow',
-  provider  => 'shell',
-  returns  => [0, 1],
+  provider => 'shell',
 }
+
