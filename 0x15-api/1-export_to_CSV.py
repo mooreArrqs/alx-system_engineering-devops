177b358de5509590170aa2959476c5fe7b165106
mooreArrqs <amandanxumalo921@gmail.com>
2024-05-20 18:38:09 -0700

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
-""" Importing module names """
+"""
+Using what yiu did in task 0, extend your Python script to export
+data in the CSV format.
+https://jsonplaceholder.typicode.com/
+
+Requirements:
+
+Records all taskthat are owned by this employee
+Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
+
+"""
 import csv
 import requests
 import sys
