d02c51786918be022458c4e4d0b580a7430b56f1
mooreArrqs <amandanxumalo921@gmail.com>
2024-05-20 19:20:31 -0700

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Extending Python script to export data in the JSON format.
+
+"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+
+    user_id = sys.argv[1]
+
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+
+    params = {"userId": user_id}
+
+    todos = requests.get(url + "todos", params=params).json()
+
+    data_to_export = {user_id: []}
+
+    for todo in todos:
+        task_info = {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": username
+        }
+        data_to_export[user_id].append(task_info)
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump(data_to_export, jsonfile, indent=4)
